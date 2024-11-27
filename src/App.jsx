8234ccb8bbbe41935641d827f59f0e830261f48a
Vitek14 <yudin.vitovt@gmail.com>
2024-11-27 10:53:00 +0300

    Replaced example with "sider" with X icon and test content text

--- src/App.jsx
+++ src/App.jsx
@@ -1,106 +1,42 @@
-import React from 'react';
-import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
-import { Breadcrumb, Layout, Menu, theme } from 'antd';
+import { Layout } from 'antd';
 import './App.css'
-const { Header, Content, Footer, Sider } = Layout;
-const items1 = ['1', '2', '3'].map((key) => ({
-  key,
-  label: `nav ${key}`,
-}));
-const items2 = [UserOutlined, LaptopOutlined, NotificationOutlined].map((icon, index) => {
-  const key = String(index + 1);
-  return {
-    key: `sub${key}`,
-    icon: React.createElement(icon),
-    label: `subnav ${key}`,
-    children: new Array(4).fill(null).map((_, j) => {
-      const subKey = index * 4 + j + 1;
-      return {
-        key: subKey,
-        label: `option${subKey}`,
-      };
-    }),
-  };
-});
+import {Header} from "antd/es/layout/layout.js";
+import { XOutlined } from "@ant-design/icons";
+const { Content, Footer, Sider } = Layout;
+
 const App = () => {
-  const {
-    token: { colorBgContainer, borderRadiusLG },
-  } = theme.useToken();
-  return (
-    <Layout style={{ minHeight: '100vh' }}>
-      <Header
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-        }}
-      >
-        <div className="demo-logo" />
-        <Menu
-          theme="dark"
-          mode="horizontal"
-          defaultSelectedKeys={['2']}
-          items={items1}
-          style={{
-            flex: 1,
-            minWidth: 0,
-          }}
-        />
-      </Header>
-      <Content
-        style={{
-          padding: '0 48px',
-        }}
-      >
-        <Breadcrumb
-          style={{
-            margin: '16px 0',
-          }}
-        >
-          <Breadcrumb.Item>Home</Breadcrumb.Item>
-          <Breadcrumb.Item>List</Breadcrumb.Item>
-          <Breadcrumb.Item>App</Breadcrumb.Item>
-        </Breadcrumb>
+    return (
         <Layout
-          style={{
-            padding: '24px 0',
-            background: colorBgContainer,
-            borderRadius: borderRadiusLG,
-          }}
-        >
-          <Sider
             style={{
-              background: colorBgContainer,
+                minHeight: "100vh"
             }}
-            width={200}
-          >
-            <Menu
-              mode="inline"
-              defaultSelectedKeys={['1']}
-              defaultOpenKeys={['sub1']}
-              style={{
-                height: '100%',
-              }}
-              items={items2}
-            />
-          </Sider>
-          <Content
-            style={{
-              padding: '0 24px',
-              minHeight: 280,
-            }}
-          >
-            Content
-          </Content>
+            >
+            <Sider width={400} style={{ background: "#fff" }}>
+                <Layout
+                        style={{
+                            marginTop: 20,
+                            marginLeft: 60
+                        }}
+                        >
+                    <Header
+                        style={{
+                            background: "#fff"
+                        }}
+                        >
+                        <XOutlined style={{ fontSize: "30px", marginLeft: -30}} />
+                    </Header>
+                    <Content
+                        style={{
+                            background: "#fff",
+                            padding: 20
+                        }}
+                        >
+                        Test
+                    </Content>
+                </Layout>
+            </Sider>
         </Layout>
-      </Content>
-      <Footer
-        style={{
-          textAlign: 'center',
-        }}
-      >
-        Ant Design ©{new Date().getFullYear()} Created by Ant UED
-      </Footer>
-    </Layout>
-  );
-};
+    )
+}
+
 export default App;
