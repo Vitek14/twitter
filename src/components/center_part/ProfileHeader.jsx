4045e96d5ad643c9e25389c2124c4ab35090fa77
Vitek14 <yudin.vitovt@gmail.com>
2024-11-28 12:55:44 +0300

    Working on profile header. Added text example with TOP alignment

--- src/components/center_part/ProfileHeader.jsx
+++ src/components/center_part/ProfileHeader.jsx
@@ -1,19 +1,31 @@
-import {Button, FloatButton, Space} from "antd";
+import {Button, FloatButton, Space, Typography} from "antd";
 import {Header} from "antd/es/layout/layout.js";
-import {FastBackwardOutlined} from "@ant-design/icons";
+import {ArrowLeftOutlined, FastBackwardOutlined} from "@ant-design/icons";
 import "./ProfileHeader.css"
 
-
+const Text = Typography;
 const ProfileHeader = () => {
     return (
         <Header style={{ background: "#fff" }}>
-            <Space direction="horizontal">
-                <Button color="default" variant="link" icon={<FastBackwardOutlined/>}
-                className="profile-headers"
+            <Space direction="horizontal"
+            style={{
+                padding: 0,
+            }}>
+                <Button color="default" variant="link" icon={<ArrowLeftOutlined
                 style={{
                     fontSize: "24px"
-                }}>
+                }}
+                />}
+                className="profile-headers">
                 </Button>
+                <Space direction="vertical" size={0} style={{
+                marginLeft: 20,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "flex-start"}}>
+                    <Text style={{ verticalAlign: "top" }}>Верхний текст</Text>
+                    <Text style={{ verticalAlign: "top" }}>Нижний текст</Text>
+                </Space>
             </Space>
         </Header>
     )
