c526ec20ba997951f908a70f8e6126154b02942d
Vitek14 <yudin.vitovt@gmail.com>
2024-12-02 23:41:59 +0300

    Added search box Fixed spaces in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,58 +4,62 @@ import SideMenu from "./components/side_menu/side_menu.jsx"
 import ProfileHeader from "./components/center_part/ProfileHeader.jsx";
 import ProfileContent from "./components/center_part/ProfileContent.jsx"
 import Post from "./components/center_part/Post.jsx"
+import SearchComponent from "./components/trending/Search.jsx";
 import {Content, Footer} from "antd/es/layout/layout.js";
 
 const App = () => {
 
-    return (
-        <Layout
-            style={{
-                minHeight: "100vh"
-            }}
-            >
-            <SideMenu/>
-            <Layout style={{ marginLeft: 400, backgroundColor: "#fff" }}>
-                <ProfileHeader/>
-                <Content style={{ margin: '0px 40px 0', width: 'calc(58%)', overflow: 'initial', border: '1px solid #eff3f4' }}>
-                    <div
-                    style={{
-                        marginTop: "0px",
-                        // padding: 24,
-                        textAlign: 'left',
-                    }}
-                    className="content-tweet"
-                    >
-                        <ProfileContent/>
-                        <Post/>
-                        <Divider/>
-                        <Post/>
-                        <Divider/>
-                        <Post/>
-                        <Divider/>
-                        <Post/>
+  return (
+    <Layout
+      style={{
+        minHeight: "100vh"
+      }}
+      >
+        <SideMenu/>
+          <Layout style={{ marginLeft: 400, backgroundColor: "#fff", border: '1px solid #000' }}>
+            <ProfileHeader/>
+              <Content style={{ margin: '0px 40px 0', width: 'calc(58%)', overflow: 'initial', border: '1px solid #eff3f4' }}>
+                <div
+                  style={{
+                    marginTop: "0px",
+                    // padding: 24,
+                    textAlign: 'left',
+                  }}
+                  className="content-tweet"
+                  >
+                    <ProfileContent/>
+                    <Post/>
+                    <Divider/>
+                    <Post/>
+                    <Divider/>
+                    <Post/>
+                    <Divider/>
+                    <Post/>
                 {/*<p>long content</p>*/}
                 </div>
                 </Content>
                 <Footer style={{
-                    position: "relative",
-                    left: "40px",
-                    marginRight: "660px",
-                    backgroundColor: "#fff",
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    textAlign: "center"
-                    // border: '1px solid #000',
+                  position: "relative",
+                  left: "40px",
+                  marginRight: "660px",
+                  backgroundColor: "#fff",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  textAlign: "center"
+                  // border: '1px solid #000',
                 }}>
-                    <Row style={{
+                  <Row style={{
                     }}>
-                        <Col>
-                            Дальше ничего нет
-                        </Col>
-                    </Row>
+                    <Col>
+                      Дальше ничего нет
+                    </Col>
+                  </Row>
                 </Footer>
-            </Layout>
+          </Layout>
+        <Layout style={{ backgroundColor: "#fff", border: '1px solid #000' }}>
+          <SearchComponent/>
+        </Layout>
         </Layout>
     )
 }
---
+++ src/components/trending/Search.jsx
@@ -0,0 +1,85 @@
+import { Row, Col, Input} from "antd";
+import SearchIcon from '@mui/icons-material/Search';
+import {useState} from "react";
+
+
+const SearchComponent = () => {
+  const [isFocused, setIsFocused] = useState(false);
+  const [searchText, setSearchText] = useState('Search');
+
+  const handleFocus = () => {
+    setIsFocused(true);
+
+    // Removes 'Search' text when user pressed on search box
+    if (searchText === 'Search') {
+      setSearchText('');
+    }
+  };
+
+  const handleBlur = () => {
+    if (searchText === '') {
+      setSearchText('Search');
+    }
+    setIsFocused(false);
+  };
+
+  const handleChange = (e) => {
+    // Sets text on search box
+    setSearchText(e.target.value);
+  };
+
+  return (
+    <div>
+      <Row
+        style={{
+          background: "#f7f9f9",
+          borderRadius: "360px",
+          width: "60%",
+          height: "44px",
+          marginLeft: "25px",
+          marginTop: "4px",
+          border: isFocused ? '1px solid #00008B' : 'none',
+        }}
+      >
+        <Col
+          style={{
+            position: "relative",
+            alignContent: "center",
+            marginLeft: "12px",
+            marginRight: "22px",
+          }}
+        >
+          <SearchIcon
+            style={{
+              color: isFocused ? '#00008B' : '#536471',
+              fontSize: "20px",
+            }}
+          />
+        </Col>
+        <Col
+          style={{
+            position: "relative",
+            alignContent: "center",
+            textAlign: "center",
+            flex: 1,
+          }}
+        >
+          <Input
+            value={searchText}
+            onFocus={handleFocus}
+            onBlur={handleBlur}  // Needs to put 'Search' text when no text is typed
+            onChange={handleChange}
+            style={{
+              border: 'none',
+              outline: 'none',
+              textAlign: "start",
+              background: 'transparent',
+            }}
+          />
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+export default SearchComponent;
