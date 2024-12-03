97c29648793ccc36346973bc912d2dcdc87efff0
Vitek14 <yudin.vitovt@gmail.com>
2024-12-03 16:58:10 +0300

    Fixed large spaces between text in <p> tags(#4) Added person name and tag

--- src/components/trending/TrendingPeople.jsx
+++ src/components/trending/TrendingPeople.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
 import {Menu, Button, Card, Row, Col, Avatar, Space, Typography} from 'antd';
+import {MoreOutlined} from "@ant-design/icons";
 
 const items = [
-  { key: '1', img: 'src/assets/Avatar.png', text: 'Text 1', button: 'Test' },
-  { key: '2', img: 'src/assets/Avatar.png', text: 'Text 2', button: 'Test' },
-  { key: '3', img: 'src/assets/Avatar.png', text: 'Text 3', button: 'Test' },
-  { key: '4', img: 'src/assets/Avatar.png', text: 'Text 4', button: 'Test' },
-  { key: '5', img: 'src/assets/Avatar.png', text: 'Text 5', button: 'Test' },
-  { key: '6', img: 'src/assets/Avatar.png', text: 'Text 6', button: 'Test' },
+  { key: '1', img: 'src/assets/Avatar.png', text: 'Typeface', secondText: "@typefaceai", button: 'Test' },
+  { key: '2', img: 'src/assets/Avatar.png', text: 'Paul Mit', secondText: "@pmitu", button: 'Test' },
+  { key: '3', img: 'src/assets/Avatar.png', text: 'Sam Altman', secondText: "@sams", button: 'Test' },
+  { key: '4', img: 'src/assets/Avatar.png', text: 'Text 4', secondText: "Text2", button: 'Test' },
+  { key: '5', img: 'src/assets/Avatar.png', text: 'Text 5', secondText: "Text2", button: 'Test' },
+  { key: '6', img: 'src/assets/Avatar.png', text: 'Text 6', secondText: "Text2", button: 'Test' },
 ];
 
 const TrendingPeople = () => {
@@ -21,40 +22,52 @@ const TrendingPeople = () => {
     <div style={{
       height: "100%",
     }}>
-      <Menu>
+      <Menu style={{
+        // marginBottom: "20px"
+      }}>
         {items.slice(0, visibleItems).map((item) => (
-          <Menu.Item key={item.key}>
-              <Row>
+          <Menu.Item key={item.key} style={{
+            marginBottom: "10px"
+          }}>
+              <Row style={{
+                // padding: "10px"
+                // position: "relative",
+                // alignItems: "center"
+              }}>
                 <Col>
-                  <Avatar src={item.img} size={40} />
+                  <Avatar src={item.img} size={42} />
                   {/*<img src={item.img} alt={item.text} style={{ width: '100%' }} />*/}
                 </Col>
-                <Col span={16} style={{
+                <Col span={10} style={{
                   marginTop: "0px",
                   alignContent: "flex-start",
                   alignItems: "flex-start",
                 }}>
-                  <Space size={0} direction="vertical" align={"start"} style={{
-
-                    // TODO: FIX SPACE BETWEEN TEXT!!! idk how to this right know, even debug tool don't show anything useful
-                    flexDirection: "row",
-                    padding: "0px",
-                    margin: "0px",
-                    marginBottom: "0px",
-                    display: "flex",
-                    alignContent: "flex-start",
-                    alignItems: "flex-start",
-                    border: "1px solid #536471"
+                  <Col style={{
+                    marginLeft: "5px",
+                    marginTop: "2px",
+                    alignItems: "start",
                   }}>
                     {/*<Typography>{item.text}</Typography>*/}
-                    <Typography.Text >
-                      <p style={{ marginTop: "0px", padding: "0px" }}>1</p>
-                      <p style={{ marginTop: "0px", padding: "0px" }}>2</p>
+                    <Typography.Text style={{ fontSize: "15px" }}>
+                      <p style={{ fontWeight: "bold", margin: 0, lineHeight: 1}}>{item.text}</p>
+                      <p style={{ color: "#536471", marginTop: 5,  lineHeight: 1}}>{item.secondText}</p>
                     </Typography.Text>
-                  </Space>
+                  </Col>
                 </Col>
                 <Col span={4}>
-                  <Button>{item.button}</Button>
+                  <Button size="large" shape="round"
+
+                    style={{
+                        color: "white",
+                        // textTextColor: "blue",
+                        backgroundColor: "black",
+                        fontWeight: "bold",
+                        fontSize: "15px"
+                    }}>
+                        Follow
+                    </Button>
+                {/*// <Button>{item.button}</Button>*/}
                 </Col>
               </Row>
           </Menu.Item>
