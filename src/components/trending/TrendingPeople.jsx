83bddd19d541535c042dcdc01cad08389c88fd4e
Vitek14 <yudin.vitovt@gmail.com>
2024-12-03 17:54:55 +0300

    Added "might like" text, changed background color. Working on trending people

--- src/components/trending/TrendingPeople.jsx
+++ src/components/trending/TrendingPeople.jsx
@@ -6,9 +6,9 @@ const items = [
   { key: '1', img: 'src/assets/Avatar.png', text: 'Typeface', secondText: "@typefaceai", button: 'Test' },
   { key: '2', img: 'src/assets/Avatar.png', text: 'Paul Mit', secondText: "@pmitu", button: 'Test' },
   { key: '3', img: 'src/assets/Avatar.png', text: 'Sam Altman', secondText: "@sams", button: 'Test' },
-  { key: '4', img: 'src/assets/Avatar.png', text: 'Text 4', secondText: "Text2", button: 'Test' },
-  { key: '5', img: 'src/assets/Avatar.png', text: 'Text 5', secondText: "Text2", button: 'Test' },
-  { key: '6', img: 'src/assets/Avatar.png', text: 'Text 6', secondText: "Text2", button: 'Test' },
+  { key: '4', img: 'src/assets/Avatar.png', text: 'Maxim Boyarchuk', secondText: "Text2", button: 'Test' },
+  { key: '5', img: 'src/assets/Avatar.png', text: 'Sweetie Fox', secondText: "@sweetie", button: 'Test' },
+  { key: '6', img: 'src/assets/Avatar.png', text: 'Gleb Sharapo', secondText: "Text2", button: 'Test' },
 ];
 
 const TrendingPeople = () => {
@@ -20,26 +20,43 @@ const TrendingPeople = () => {
 
   return (
     <div style={{
-      height: "100%",
+      // height: "100%",
+      marginTop: "16px",
+      marginLeft: "25px",
+      width: "356px",
+      backgroundColor: "#f7f9f9",
+      borderRadius: "16px"
     }}>
+      {/* might like text */}
+      <Typography.Text style={{
+        margin: "16px",
+        marginTop: "16px",
+        marginLeft: "16px",
+        fontWeight: "bold",
+        fontSize: "20px"
+      }}>
+        {/* P tags causes unexpected spaces...*/}
+        You might like
+      </Typography.Text>
       <Menu style={{
+        marginTop: "16px",
+        backgroundColor: "#f7f9f9",
+        borderRadius: "16px",
+        width: "356px"
         // marginBottom: "20px"
       }}>
         {items.slice(0, visibleItems).map((item) => (
           <Menu.Item key={item.key} style={{
-            marginBottom: "10px"
+            marginBottom: "24px"
           }}>
-              <Row style={{
-                // padding: "10px"
-                // position: "relative",
-                // alignItems: "center"
-              }}>
+              <Row>
                 <Col>
                   <Avatar src={item.img} size={42} />
                   {/*<img src={item.img} alt={item.text} style={{ width: '100%' }} />*/}
                 </Col>
-                <Col span={10} style={{
-                  marginTop: "0px",
+                <Col span={13} style={{
+                  marginTop: "2px",
+                  marginRight: "10px",
                   alignContent: "flex-start",
                   alignItems: "flex-start",
                 }}>
@@ -55,19 +72,19 @@ const TrendingPeople = () => {
                     </Typography.Text>
                   </Col>
                 </Col>
-                <Col span={4}>
+                <Col>
+
+                  {/* Follow button */}
                   <Button size="large" shape="round"
 
                     style={{
                         color: "white",
-                        // textTextColor: "blue",
                         backgroundColor: "black",
                         fontWeight: "bold",
                         fontSize: "15px"
                     }}>
                         Follow
                     </Button>
-                {/*// <Button>{item.button}</Button>*/}
                 </Col>
               </Row>
           </Menu.Item>
