fc74b756561e635be207e42f54c91921b3288071
Vitek14 <yudin.vitovt@gmail.com>
2024-12-08 17:38:46 +0300

    Added files to light and dark themes

---
+++ src/themes/DarkTheme.jsx
@@ -0,0 +1,17 @@
+import { theme } from 'antd';
+
+const darkTheme =
+  {
+  algorithm: theme.darkAlgorithm,
+  token: {
+    colorBgBase: "#15202B",
+    components: {
+      Row: {
+        colorPrimary: "#000"
+      },
+
+    }
+  },
+};
+
+export default darkTheme;
