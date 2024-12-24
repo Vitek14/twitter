1fe12bf6a22e0ba06a21bf5aff5fce3e5637a711
Vitek14 <yudin.vitovt@gmail.com>
2024-12-24 18:16:23 +0300

    Removed unused imports from HomePage.jsx Removed ConfigProvider from HomePage.jsx(#5)

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,26 +1,15 @@
-import { ConfigProvider, Layout, theme} from "antd";
+import { Layout } from "antd";
 import ControlPanel from "../components/common_components/control_panel/ControlPanel.jsx";
-import { Tabs } from 'antd';
-import LightTheme from "../themes/LightTheme.jsx";
 import Home from "../components/home/Home.jsx";
-import DarkTheme from "../themes/DarkTheme.jsx";
-
-const { Header } = Layout;
-const {TabPane} = Tabs;
 
 const HomePage = () => {
-  const {
-    token: { colorBgContainer },
-  } = theme.useToken();
 
   return (
     <>
-      <ConfigProvider theme={LightTheme}>
         <Layout style={{ display: "flex", minHeight: '100vh' }}>
           <ControlPanel />
           <Home/>
         </Layout>
-      </ConfigProvider>
     </>
   )
 }
