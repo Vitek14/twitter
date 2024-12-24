5e8004d5abd44636b70887c8f7c526cabda28ef0
Vitek14 <yudin.vitovt@gmail.com>
2024-12-24 21:17:08 +0300

    Removed components from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from "./App.jsx";
-import {ConfigProvider} from "antd";
-import LightTheme from "./themes/LightTheme.jsx";
-import DarkTheme from "./themes/DarkTheme.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ConfigProvider theme={LightTheme}>
-      <App/>
-    </ConfigProvider>
-  </StrictMode>,
+    <App/>
+  </StrictMode>
 )
