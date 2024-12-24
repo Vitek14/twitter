e999e0e3ae1c14611bdc1d3c6364931f900a5da3
Vitek14 <yudin.vitovt@gmail.com>
2024-12-24 18:17:15 +0300

    Added ConfigProvider to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from "./App.jsx";
+import {ConfigProvider} from "antd";
+import LightTheme from "./themes/LightTheme.jsx";
+import DarkTheme from "./themes/DarkTheme.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App/>
+    <ConfigProvider theme={LightTheme}>
+      <App/>
+    </ConfigProvider>
   </StrictMode>,
 )
