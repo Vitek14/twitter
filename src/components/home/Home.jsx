ff0b0d91465edfedc9e09c8277ee651826ae4470
Vitek14 <yudin.vitovt@gmail.com>
2024-12-29 15:50:57 +0300

    Added Recent posts button Added Posts Added recommendation panel to Home page

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,19 +3,29 @@ import ContentTabs from "./components/ContentTabs.jsx";
 const { Header, Content } = Layout;
 import data from "../../data/data.json"
 import NewPost from "./components/NewPost.jsx";
+import RecentPosts from "./components/RecentPosts.jsx";
+import Post from "../profile/components/Post.jsx";
 
 const Home = () => {
-  const {
-    token: { colorBgContainer },
-  } = theme.useToken();
 
   return (
-    <Layout style={{ maxWidth: "600px" }}>
-      <Header style={{ backgroundColor: colorBgContainer, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+    <Layout style={{ maxWidth: "800px" }}>
+      <Header style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
         <ContentTabs/>
       </Header>
-      <Content style={{ backgroundColor: colorBgContainer }}>
+      <Content>
         <NewPost profile_image={data.profile.avatar} />
+        <RecentPosts/>
+        {data.posts.map((post, key) => {
+            // const user = data.users.find(u => u.id === post.user_id);
+            return(
+              <Post
+                key={key}
+                post={post}
+                user={data.users.find(u => u.id === post.user_id)}
+              />
+            )
+          })}
       </Content>
     </Layout>
   )
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,10 @@
 import { Layout } from "antd";
 import ControlPanel from "../components/common_components/control_panel/ControlPanel.jsx";
 import Home from "../components/home/Home.jsx";
+import Sider from "antd/es/layout/Sider.js";
+import RecommendationPeople from "../components/common_components/recommendation_panel/RecommendationPeople.jsx";
+import RecommendationTopics from "../components/common_components/recommendation_panel/RecommendationTopics.jsx";
+import SearchBar from "../components/common_components/recommendation_panel/SearchBar.jsx";
 
 const HomePage = () => {
 
@@ -9,6 +13,11 @@ const HomePage = () => {
         <Layout style={{ display: "flex", minHeight: '100vh' }}>
           <ControlPanel />
           <Home/>
+          <Sider >
+            <SearchBar/>
+            <RecommendationPeople/>
+            <RecommendationTopics/>
+          </Sider>
         </Layout>
     </>
   )
