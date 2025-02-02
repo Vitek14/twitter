3368ee9f95d66656111a9472ea19b3d065455a9d
Vitek14 <yudin.vitovt@gmail.com>
2025-02-02 15:02:41 +0300

    Added backend info for recommendation people panel Fix: border radius now set to antd element

--- src/components/common_components/recommendation_panel/RecommendationPanel.jsx
+++ src/components/common_components/recommendation_panel/RecommendationPanel.jsx
@@ -5,6 +5,7 @@ import RecommendationTopics from "./RecommendationTopics.jsx";
 
 
 const RecommendationPanel = () => {
+
   return (
     <Layout style={{ width: "100%", position: 'fixed' }}>
       <SearchBar/>
--- src/components/common_components/recommendation_panel/RecommendationPeople.jsx
+++ src/components/common_components/recommendation_panel/RecommendationPeople.jsx
@@ -1,17 +1,33 @@
-import { useState } from 'react';
+import {useEffect, useState} from 'react';
 import {Menu, Button, Row, Col, Avatar, Typography} from 'antd';
 import VerifiedIcon from '@mui/icons-material/Verified';
+import {CheckCircleTwoTone} from "@ant-design/icons";
+import axios from "axios";
 
-const items = [
-  { key: '1', img: 'src/assets/avatars/typeface.png', text: 'Typeface', secondText: "@typefaceai", button: 'Test' },
-  { key: '2', img: 'src/assets/avatars/paul.png', text: 'Paul Mit', secondText: "@pmitu", button: 'Test' },
-  { key: '3', img: 'src/assets/avatars/sam.png', text: 'Sam Altman', secondText: "@sams", button: 'Test' },
-  { key: '4', img: 'src/assets/avatars/maxim.png', text: 'Maxim Boyarchuk', secondText: "@maximka", button: 'Test' },
-  { key: '5', img: 'src/assets/avatars/sweetie.jpg', text: 'Sweetie Fox', secondText: "@sweetie", button: 'Test' },
-  { key: '6', img: 'src/assets/avatars/gleb.png', text: 'Gleb Sharapo', secondText: "@sharapogleb", button: 'Test' },
-];
+// const items = [
+//   { key: '1', img: 'src/assets/avatars/typeface.png', text: 'Typeface', secondText: "@typefaceai", button: 'Test' },
+//   { key: '2', img: 'src/assets/avatars/paul.png', text: 'Paul Mit', secondText: "@pmitu", button: 'Test' },
+//   { key: '3', img: 'src/assets/avatars/sam.png', text: 'Sam Altman', secondText: "@sams", button: 'Test' },
+//   { key: '4', img: 'src/assets/avatars/maxim.png', text: 'Maxim Boyarchuk', secondText: "@maximka", button: 'Test' },
+//   { key: '5', img: 'src/assets/avatars/sweetie.jpg', text: 'Sweetie Fox', secondText: "@sweetie", button: 'Test' },
+//   { key: '6', img: 'src/assets/avatars/gleb.png', text: 'Gleb Sharapo', secondText: "@sharapogleb", button: 'Test' },
+// ];
 
 const RecommendationPeople = () => {
+
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/api/users/might_like/")
+      .then(res => {
+        setItems(res.data);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }, []);
+
   const [visibleItems, setVisibleItems] = useState(3);
 
   const showMore = () => {
@@ -51,7 +67,7 @@ const RecommendationPeople = () => {
                 <Col style={{
                   alignSelf: "flex-start"
                 }}>
-                  <Avatar src={item.img} size={53} />
+                  <Avatar src={item.avatar_url} size={53} />
                 </Col>
                 <Col style={{
                   marginTop: "2px",
@@ -63,9 +79,11 @@ const RecommendationPeople = () => {
                     marginTop: "2px",
                     alignItems: "start",
                   }}>
-                    <Typography.Text style={{ fontSize: "20px" }}>
-                      <p style={{ fontWeight: "bold", margin: 0, lineHeight: 1}}>{item.text}</p>
-                      <p style={{ color: "#536471", marginTop: 5,  lineHeight: 1}}>{item.secondText}</p>
+                    <Typography.Text style={{fontSize: "20px"}}>
+                      {/*<p style={{ fontWeight: "bold", margin: 0, lineHeight: 1}}>{item.text}</p>*/}
+                      {/*<p style={{ color: "#536471", marginTop: 5,  lineHeight: 1}}>{item.secondText}</p>*/}
+                      <p style={{fontWeight: "bold", margin: 0, lineHeight: 1}}>{item.first_name}</p>
+                      <p style={{color: "#536471", marginTop: 5, lineHeight: 1}}>{item.last_name}</p>
                     </Typography.Text>
                   </Col>
                 </Col>
@@ -74,10 +92,10 @@ const RecommendationPeople = () => {
                   height: "auto",
                   marginLeft: "5px",
                 }} span={4}>
-                  <VerifiedIcon style={{
+                  {item.verified && <VerifiedIcon style={{
                     color: "#1D9BF0",
                     fontSize: "22px",
-                  }}/>
+                  }} />}
                 </Col>
                 <Col style={{ marginLeft: "auto", alignSelf: "flex-start", marginTop: "8px" }}>  {/* Сомнительное решение. */}
                   <Button size="large" shape="round"
