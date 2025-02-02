7b528994a6bd4871f5404e650073daebb690111e
Vitek14 <yudin.vitovt@gmail.com>
2025-02-02 13:18:01 +0300

    Fix: fixed verified icon being in middle of layout in overview

--- src/components/profile/components/Overview.jsx
+++ src/components/profile/components/Overview.jsx
@@ -1,15 +1,21 @@
-import {Button, Space, Typography, Layout } from "antd";
-import {ArrowLeftOutlined, CheckCircleTwoTone } from "@ant-design/icons";
-import "../profile.scss"
+import { Button, Space, Typography, Layout } from "antd";
+import { ArrowLeftOutlined, CheckCircleTwoTone } from "@ant-design/icons";
+import "../profile.scss";
 
 const { Header } = Layout;
-const {Text} = Typography;  // THIS SHOULD BE CURLY BRACES!
-const Overview = ({profile}) => {
+const { Text } = Typography;
+
+const Overview = ({ profile }) => {
   return (
     <div className="overview">
       <Header className="overview__header">
-        <Space direction="horizontal">
-          <Button color="default" variant="link" icon={<ArrowLeftOutlined/>} className="profile-headers"/>
+        <Space className="overview__space-wrapper" direction="horizontal" align="start">
+          <Button
+            color="default"
+            variant="link"
+            icon={<ArrowLeftOutlined />}
+            className="profile-headers"
+          />
           <Space className="overview__space" direction="vertical" size={0}>
             <Text className="overview__fullname">
               {profile.first_name}
@@ -17,11 +23,11 @@ const Overview = ({profile}) => {
             </Text>
             <Text className="overview__view-counter">23 post</Text>
           </Space>
-          <CheckCircleTwoTone/>
+          <CheckCircleTwoTone className="profile-check" />
         </Space>
       </Header>
     </div>
-  )
-}
+  );
+};
 
-export default Overview
+export default Overview;
