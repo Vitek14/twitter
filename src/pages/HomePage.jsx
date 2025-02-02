786c03205aee3b8470e2255cb33eef46bfdfe6a2
Vitek14 <yudin.vitovt@gmail.com>
2025-02-02 13:35:22 +0300

    Fixed control panel error when switching pages

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,13 +5,27 @@ import Sider from "antd/es/layout/Sider.js";
 import RecommendationPeople from "../components/common_components/recommendation_panel/RecommendationPeople.jsx";
 import RecommendationTopics from "../components/common_components/recommendation_panel/RecommendationTopics.jsx";
 import SearchBar from "../components/common_components/recommendation_panel/SearchBar.jsx";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 const HomePage = () => {
+  const [profile, setProfile] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/api/profile/")
+      .then(res => {
+        setProfile(res.data);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }, []);
 
   return (
     <>
         <Layout style={{ display: "flex", minHeight: '100vh' }}>
-          <ControlPanel />
+          <ControlPanel profile={profile} />
           <Home/>
           <Sider >
             <SearchBar/>
--- src/pages/MessagePage.jsx
+++ src/pages/MessagePage.jsx
@@ -2,16 +2,31 @@ import { Layout, theme} from "antd";
 import ControlPanel from "../components/common_components/control_panel/ControlPanel.jsx";
 import Message from "../components/message/Message.jsx";
 import Select from "../components/message/components/Select.jsx";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 const { Sider } = Layout
 
 const MessagePage = () => {
+  const [profile, setProfile] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/api/profile/")
+      .then(res => {
+        setProfile(res.data);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }, []);
+
   const {
     token: { colorBgContainer },
   } = theme.useToken();
   return (
       <Layout style={{ display: "flex", minHeight: '100vh' }}>
-        <ControlPanel/>
+        <ControlPanel profile={profile}/>
         <Message/>
         <Sider width={"48.2vw"} style={{
           borderLeft: "1px solid #f7f9f9",
