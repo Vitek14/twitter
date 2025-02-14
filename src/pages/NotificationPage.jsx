2bee7efa5c2019b35a1ff5e7324f72e546e704d3
Vitek14 <yudin.vitovt@gmail.com>
2025-02-14 11:38:32 +0300

    Implementing notifications

--- src/pages/NotificationPage.jsx
+++ src/pages/NotificationPage.jsx
@@ -1,38 +1,76 @@
-import {Layout, Menu, Typography} from "antd"
+import {Avatar, Card, Layout, Menu, Typography} from "antd"
 import "./notificationpage.scss"
 import {XOutlined} from "@ant-design/icons";
 import ControlPanel from "../components/common_components/new_control_panel/ControlPanel.jsx";
+import {useEffect, useState} from "react";
+import Api from "../api.js";
+import {useNavigate} from "react-router-dom";
 
 const {Sider, Content,  Header, Footer} = Layout;
 
 const NotificationPage = () => {
+  const navigate = useNavigate();
+  const [notifications, setNotifications] = useState([]);
+
+  const formatDate = (dateString) => {
+    const options = { year: 'numeric', month: 'short', day: 'numeric' }; // Пример: Feb 10, 2025
+    return new Date(dateString).toLocaleDateString('en-US', options);
+  };
+
+  useEffect(() => {
+    Api.Notifications.get().then((res) => {
+      setNotifications(res.data);
+    }).catch(err => {
+      localStorage.deleteItem('token');
+      navigate("/login");
+      console.error(err);
+    });
+  }, []);
+
   return (
     <div className="main">
       {/* Левый Sider */}
-      <div className="control-panel" style={{flexGrow: 1, display: 'flex', alignItems: "flex-end", flexDirection: 'column', height: "100vh"}}>
+      <div className="main__control-panel">
         <ControlPanel />
       </div>
 
       {/* Центральный Content */}
-      <div style={{flexGrow: 2, display: 'flex', flexDirection: 'column'}}>
+      <div className="main__content-box">
         <Layout>
-          <Header style={{textAlign: 'start', padding: '16px'}}>
+          <Header className="main__content-box__header">
             <Typography.Title level={4}>
               Notifications
             </Typography.Title>
           </Header>
-          <Content style={{padding: '24px', flexGrow: 1}}>
-            <div style={{
-              background: '#fafafa',
-              height: '100%',
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center'
-            }}>
-              Основное содержимое
+          <Content className="main__content-box__content">
+            <div className="main__content-box__content__container">
+              {notifications.map((notification) => (
+                 <Card
+                   hoverable
+                   key={notification.parent_id}
+                   onClick={() => handlePostClick(notification.parent_id)}
+                   style={{ width: "100%", marginBottom: '10px' }}
+                >
+                  <Card.Meta
+                    avatar={<Avatar src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/X_logo.jpg/1200px-X_logo.jpg" />}
+                    title={notification.type}
+                    description={
+                      <>
+                        {notification.type === "Login" ? (
+                          <p>
+                            There was a login to your account from a new device on {formatDate(notification.createdAt)}. Review it now.
+                          </p>
+                        ) : (
+                          <p>Новый вход от вашего имени в x.com: 2025-12-02</p>
+                        )}
+                      </>
+                    }
+                  />
+                </Card>
+              ))}
             </div>
           </Content>
-          <Footer style={{background: '#eee', textAlign: 'center'}}>Ant Design ©2025</Footer>
+          <Footer style={{textAlign: 'center'}}>x.com</Footer>
         </Layout>
       </div>
 
